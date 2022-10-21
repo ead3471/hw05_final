04c27ae3847df4e22dfa99a80d54dcf4c22b9db3
Gubarik <ead3471@gmail.com>
2022-10-21 23:42:02 +0300

    urls+views cleanup

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -4,59 +4,51 @@ from . import views
 app_name = 'posts'
 
 urlpatterns = [
-    #path('', views.index, name='index'),
+    # Последние обновления
     path('', views.IndexPageView.as_view(), name='index'),
-    #path("group/<slug:slug>/", views.group_posts, name='group_list'),
-    path("group/<slug:slug>/", views.GroupPageView.as_view(), name='group_list'),
-    
+
+    # Посты группы
+    path("group/<slug:slug>/",
+         views.GroupPageView.as_view(),
+         name='group_list'),
+
     # Профайл пользователя
-    #path("profile/<str:username>/", views.profile, name='profile'),
     path("profile/<str:username>/",
          views.ProfilePageView.as_view(),
          name='profile'),
 
     # Просмотр записи
-    #path('posts/<int:post_id>/', views.post_detail, name='post_detail'),
-    path('posts/<int:post_id>/', views.PostDetailView.as_view(), name='post_detail'),
+    path('posts/<int:post_id>/',
+         views.PostDetailView.as_view(),
+         name='post_detail'),
+
     # Добавление записи
-    #path('create/', views.post_create, name='post_create'),
     path('create/', views.PostCreateView.as_view(), name='post_create'),
+
     # Редактирование записи
-    #path('posts/<int:post_id>/edit/', views.post_edit, name='post_edit'),
-    path('posts/<int:pk>/edit/', views.PostEditView.as_view(), name='post_edit'),
+    path('posts/<int:pk>/edit/',
+         views.PostEditView.as_view(),
+         name='post_edit'),
+
     # Добавление поста
-    # path(
-    #     'posts/<int:post_id>/comment/',
-    #     views.add_comment,
-    #     name='add_comment'),
     path(
         'posts/<int:post_id>/comment/',
         views.AddCommentView.as_view(),
         name='add_comment'),
+
     # Страница постов из подписок
-    # path('follow/',
-    #      views.follow_index,
-    #      name='follow_index'),
     path('follow/',
          views.FollowIndexView.as_view(),
          name='follow_index'),
+
     # Зарегестрировать подписку на автора
-    # path(
-    #     'profile/<str:username>/follow/',
-    #     views.profile_follow,
-    #     name='profile_follow'
-    # ),
     path(
         'profile/<str:username>/follow/',
         views.FollowView.as_view(),
         name='profile_follow'
     ),
+
     # Отписаться от автора
-    # path(
-    #     'profile/<str:username>/unfollow/',
-    #     views.profile_unfollow,
-    #     name='profile_unfollow'
-    # ),
     path(
         'profile/<str:username>/unfollow/',
         views.UnfollowView.as_view(),
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,9 +1,7 @@
-from multiprocessing import get_context
-from django.shortcuts import redirect, render, get_object_or_404
+from django.shortcuts import redirect, get_object_or_404
 from .models import Follow, Post, Group, Comment
 from .forms import PostForm, CommentForm
 from django.contrib.auth import get_user_model
-from django.contrib.auth.decorators import login_required
 from .utils import get_page
 from django.utils import timezone
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -13,17 +11,6 @@ from django.views.generic.edit import BaseCreateView
 from django.urls import reverse
 
 User = get_user_model()
-
-
-# def index(request):
-#     POSTS_PER_PAGE = 10
-#     template = 'posts/index.html'
-#     posts = Post.objects.all()
-#     context = {
-#         'page_obj': get_page(request, posts, POSTS_PER_PAGE)
-#     }
-
-#     return render(request, template, context)
 
 
 class IndexPageView(ListView):
@@ -38,21 +25,6 @@ class IndexPageView(ListView):
                                        Post.objects.all(),
                                        10)
         return context
-
-
-# def group_posts(request, slug):
-#     POSTS_PER_PAGE = 10
-#     template = 'posts/group_list.html'
-#     group = get_object_or_404(Group, slug=slug)
-#     title = f'Записи сообщества {group.title}'
-#     posts = group.posts.all()
-#     context = {
-#         'title': title,
-#         'group': group,
-#         'page_obj': get_page(request, posts, POSTS_PER_PAGE)
-#     }
-
-#     return render(request, template, context)
 
 
 class GroupPageView(ListView):
@@ -71,24 +43,6 @@ class GroupPageView(ListView):
         context = super().get_context_data(**kwargs)
         context['group'] = self.group
         return context
-
-
-# def profile(request, username):
-#     POSTS_PER_PAGE = 10
-#     profile_user = get_object_or_404(User, username=username)
-#     posts = profile_user.posts.all()
-
-#     profile_user_is_in_followings = (Follow.
-#                                      objects.
-#                                      filter(user__id=request.user.id).
-#                                      filter(author=profile_user).exists())
-
-#     context = {
-#         'author': profile_user,
-#         'page_obj': get_page(request, posts, POSTS_PER_PAGE),
-#         'following': profile_user_is_in_followings
-#     }
-#     return render(request, 'posts/profile.html', context)
 
 
 class ProfilePageView(ListView):
@@ -119,17 +73,6 @@ class ProfilePageView(ListView):
         return context
 
 
-
-# def post_detail(request, post_id):
-#     post = get_object_or_404(Post, pk=post_id)
-#     context = {
-#         'post': post,
-#         'comments': post.comments.all(),
-#         'form': CommentForm()
-#     }
-
-#     return render(request, 'posts/post_detail.html', context)
-
 class PostDetailView(DetailView):
     template_name = 'posts/post_detail.html'
     model = Post
@@ -141,8 +84,6 @@ class PostDetailView(DetailView):
         context["form"] = CommentForm()
         context['comments'] = self.get_object().comments.all()
         return context
-
-
 
 
 class PostCreateView(LoginRequiredMixin, CreateView):
@@ -159,35 +100,18 @@ class PostCreateView(LoginRequiredMixin, CreateView):
         return redirect('posts:profile', username=self.request.user.username)
 
 
-
-# @login_required
-# def post_create(request):
-#     form = PostForm(request.POST or None)
-
-#     if request.method == "POST":
-#         if form.is_valid():
-#             post = form.save(commit=False)
-#             post.created = timezone.now()
-#             post.author = request.user
-#             post.save()
-#             return redirect('posts:profile', username=request.user.username)
-
-#     return render(request,
-#                   'posts/create_post.html',
-#                   {'form': form})
-
 class PostEditView(LoginRequiredMixin, UpdateView):
     model = Post
     template_name = 'posts/create_post.html'
-    fields = ['text','image','group']
+    fields = ['text', 'image', 'group']
 
     def get_success_url(self):
         return reverse('posts:post_detail', args=(self.get_object().pk,))
 
-    def get(self,request,pk):
+    def get(self, request, pk):
         if self.get_object().author != request.user:
             return redirect('posts:post_detail', post_id=self.get_object().pk)
-        return super().get(request,pk)
+        return super().get(request, pk)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -200,39 +124,6 @@ class PostEditView(LoginRequiredMixin, UpdateView):
 
         form.save()
         return redirect('posts:post_detail', post_id=self.get_object().pk)
-
-# @login_required
-# def post_edit(request, post_id):
-#     post = get_object_or_404(Post, pk=post_id)
-#     if post.author != request.user:
-#         return redirect('posts:post_detail', post_id=post.pk)
-
-#     form = PostForm(request.POST or None,
-#                     files=request.FILES or None,
-#                     instance=post)
-#     if request.method == "POST":
-#         if form.is_valid():
-#             form.save()
-#             return redirect('posts:post_detail', post_id=post.pk)
-
-#     return render(request,
-#                   'posts/create_post.html',
-#                   {'form': form,
-#                    'is_edit': True})
-
-
-# @login_required
-# def add_comment(request, post_id):
-#     form = CommentForm(request.POST or None)
-#     post = get_object_or_404(Post, pk=post_id)
-#     if request.method == 'POST':
-#         if form.is_valid():
-#             comment = form.save(commit=False)  # type: Comment
-#             comment.author = request.user
-#             comment.created = timezone.now()
-#             comment.post = post
-#             comment.save()
-#             return redirect('posts:post_detail', post_id=post.pk)
 
 
 class AddCommentView(LoginRequiredMixin, BaseCreateView):
@@ -250,19 +141,6 @@ class AddCommentView(LoginRequiredMixin, BaseCreateView):
         return redirect('posts:post_detail', post_id=post.pk)
 
 
-
-# @login_required
-# def profile_follow(request, username):
-#     author = get_object_or_404(User, username=username)
-#     follow_exist = request.user.id in (author.
-#                                        following
-#                                        .all()
-#                                        .values_list('user', flat=True))
-#     if author != request.user and not follow_exist:
-#         Follow.objects.create(user=request.user, author=author)
-#     return redirect('posts:profile', username=author)
-
-
 class FollowView(LoginRequiredMixin, View):
     def get(self, request, username):
 
@@ -274,27 +152,6 @@ class FollowView(LoginRequiredMixin, View):
         if author != request.user and not follow_exist:
             Follow.objects.create(user=request.user, author=author)
         return redirect('posts:profile', username=username)
-
-
-# @login_required
-# def follow_index(request):
-#     POSTS_PER_PAGE = 10
-#     user_follows_authors_ids = (request.
-#                                 user.
-#                                 follower.
-#                                 values_list("author__id", flat=True))
-
-#     following_authors_posts = (Post.
-#                                objects.
-#                                filter(
-#                                    author__id__in=user_follows_authors_ids))
-
-#     context = {
-#         'page_obj': get_page(request,
-#                              following_authors_posts,
-#                              POSTS_PER_PAGE)
-#     }
-#     return render(request, 'posts/follow.html', context)
 
 
 class FollowIndexView(LoginRequiredMixin, ListView):
@@ -326,15 +183,6 @@ class FollowIndexView(LoginRequiredMixin, ListView):
                                                      self.paginate_by)
         return context
 
-
-
-
-@login_required
-def profile_unfollow(request, username):
-    author = get_object_or_404(User, username=username)
-    follows = Follow.objects.filter(user=request.user, author=author)
-    follows.delete()
-    return redirect('posts:profile', username=author)
 
 class UnfollowView(LoginRequiredMixin, View):
     def get(self, request, username):
